8be9eb06a930066fa9ee2f36d939b9b48f58d44a
qiang <2467558421@qq.com>
2021-08-13 15:06:14 +0800

    用vue-cli3搭建组件开发环境

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+'use strict'
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+module.exports = {
+  // 将 examples 目录添加为新的页面
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'examples/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 输出文件名
+      filename: 'index.html'
+    }
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    resolve: {
+      alias: {
+        '@': resolve('examples'),
+        vendor: path.resolve(__dirname, '../examples/vendor')
+      }
+    }
+  }
+}
